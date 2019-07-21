40577ad187c2d04f050d51e319a7ab2f1148d64c
robert-barcelona <robertanthony.bcn@gmail.com>
2019-07-21 18:16:06 +0200

    various changes to fix interaction with front

--- logic/index.js
+++ logic/index.js
@@ -13,21 +13,19 @@ module.exports = {
     try {
       startTime = Date.parse(start)
       endTime = Date.parse(end)
-
     } catch (e) {
       throw new Error('Error in start or end times given')
     }
 
     return data.filter(room => {
-
       const bookedStart = Date.parse(room.bookingstart)
       const bookedEnd = bookedStart + room.bookinglength * (1000 * 60 * 60 * 24)
       const overlap = !(startTime < bookedStart && endTime < bookedEnd) || (startTime > bookedEnd)
       return (room.people >= people && !overlap)
     }).map(room => {
       if (discount) {
-        console.log(discount)
-        room.cost *= 1 - discount
+        console.log(`discount is ${discount}`)
+        room.discountedCost = (room.cost * (1 - discount/100)).toFixed(2);
         room.discount = discount
       }
       return room
